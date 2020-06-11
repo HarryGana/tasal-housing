0ea137c940f707b33fb32aa7882478814cda5cd6
Haruna Gana <harrygana@hotmail.com>
2020-06-11 20:08:15 +0100

    made corrections

--- src/TasalHousing.web/Startup.cs
+++ src/TasalHousing.web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace TasalHousing.web
 {
@@ -24,9 +25,9 @@ namespace TasalHousing.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<AuthenticationDbContext>(
-                options => option.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
+            services.AddDbContextPool<AuthenticationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
