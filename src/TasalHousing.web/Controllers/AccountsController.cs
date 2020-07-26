d174f012667f33c0e84d822858ddd95fcb3930db
Haruna Gana <harrygana@hotmail.com>
2020-07-26 23:53:44 +0100

    added models for login/register

--- src/TasalHousing.web/Controllers/AccountsController.cs
+++ src/TasalHousing.web/Controllers/AccountsController.cs
@@ -1,17 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using TasalHousing.web.Models;
 
 namespace TasalHousing.Web.Controllers
-{
+{    
     public class AccountsController : Controller
     {
+        [HttpGet]
+
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        
+        public IActionResult Login(LoginModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            throw new NotImplementedException();
         }
     }
 }
---
+++ src/TasalHousing.web/Models/RegisterModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasalHousing.web.Models
+{
+    public class RegisterModel
+    {
+        [DisplayName("Name")]
+        [Required]
+        
+        public string FullName { get; set; }
+
+        [DisplayName("Email Address")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+       
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+        [DisplayName("Confirmed Password")]
+        [Required]
+        [Compare(nameof(Password))]
+
+        public string ConfirmPassword { get; set; }
+    }
+}
